1b5f1f940c7687966f8135814ab90baf09d82340
Yousef Zaher <yzaher.uk@gmail.com>
2022-11-08 21:52:55 +0000

    :sparkles: added old response manager and menus

---
+++ qalib/__init__.py
@@ -0,0 +1,16 @@
+"""
+Discord API Wrapper
+
+
+A basic wrapper for the Discord API.
+:copyright: (c) 2015-present Rapptz
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = 'qalib'
+__author__ = 'YousefEZ'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2022-present YousefEZ'
+__version__ = '0.0.1'
+
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
