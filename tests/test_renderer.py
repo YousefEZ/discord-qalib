b3db8abc8e3225dadf9f5f8ba3fa08546d83826b
Yousef Zaher <yzaher.uk@gmail.com>
2022-11-11 23:35:23 +0000

    :test_tube: add test to test full capabilities

--- tests/test_renderer.py
+++ tests/test_renderer.py
@@ -2,6 +2,7 @@ import unittest
 
 import qalib._xml_renderer as xml_renderer
 
+import datetime
 
 class TestRenderer(unittest.TestCase):
     """Tests the XML Renderer"""
@@ -10,3 +11,9 @@ class TestRenderer(unittest.TestCase):
         renderer = xml_renderer.Renderer(path)
         embed = renderer.render("Launch")
         self.assertEqual(embed.title, "Hello World")
+
+    def test_full_render(self):
+        path = "tests/routes/full_embeds.xml"
+        renderer = xml_renderer.Renderer(path)
+        embed = renderer.render("test_key", todays_date=datetime.datetime.now())
+        self.assertEqual(embed.title, "Test")
