653d486b4ed3495aa8b853d691340df75a259228
Yousef Zaher <yzaher.uk@gmail.com>
2022-11-12 18:37:04 +0000

    :test_tube: added more tests

---
+++ tests/test_menu_renderer.py
@@ -0,0 +1,32 @@
+import unittest
+
+from qalib._xml_renderer import MenuRenderer
+
+
+class MenuRendererTest(unittest.TestCase):
+
+    def test_render(self):
+        path = "tests/routes/menus.xml"
+        renderer = MenuRenderer(path, "Menu1")
+        embed = renderer.render("test_key1")
+        self.assertEqual(embed.title, "Hello World")
+
+    def test_full_render(self):
+        path = "tests/routes/menus.xml"
+        renderer = MenuRenderer(path, "Menu1")
+        embed = renderer.render("test_key2")
+        self.assertEqual(embed.title, "Hello Planet")
+
+    def test_key_not_exist(self):
+        path = "tests/routes/menus.xml"
+        self.assertRaises(KeyError, MenuRenderer, path, "not_a_key")
+
+    def test_pages(self):
+        path = "tests/routes/menus.xml"
+        renderer = MenuRenderer(path, "Menu1")
+        self.assertEqual(renderer.number_of_pages, 2)
+
+    def test_keys(self):
+        path = "tests/routes/menus.xml"
+        renderer = MenuRenderer(path, "Menu1")
+        self.assertEqual(renderer.keys, ["test_key1", "test_key2"])
--- tests/test_renderer.py
+++ tests/test_renderer.py
@@ -8,12 +8,17 @@ class TestRenderer(unittest.TestCase):
     """Tests the XML Renderer"""
     def test_render(self):
         path = "tests/routes/simple_embeds.xml"
-        renderer = xml_renderer.Renderer(path)
+        renderer = xml_renderer.EmbedRenderer(path)
         embed = renderer.render("Launch")
         self.assertEqual(embed.title, "Hello World")
 
     def test_full_render(self):
         path = "tests/routes/full_embeds.xml"
-        renderer = xml_renderer.Renderer(path)
+        renderer = xml_renderer.EmbedRenderer(path)
         embed = renderer.render("test_key", todays_date=datetime.datetime.now())
         self.assertEqual(embed.title, "Test")
+
+    def test_key_not_exist(self):
+        path = "tests/routes/full_embeds.xml"
+        renderer = xml_renderer.EmbedRenderer(path)
+        self.assertRaises(KeyError, renderer.render, "not_a_key")
