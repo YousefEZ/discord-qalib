7825201970a5227af6d407455802f8b19d0e765b
Yousef Zaher <yzaher.uk@gmail.com>
2022-11-15 23:41:38 +0000

    :white_check_mark: included JSON tests

--- tests/test_menu_renderer.py
+++ tests/test_menu_renderer.py
@@ -5,28 +5,55 @@ from qalib import MenuRenderer
 
 class MenuRendererTest(unittest.TestCase):
 
-    def test_render(self):
+    def test_xml_render(self):
         path = "tests/routes/menus.xml"
         renderer = MenuRenderer(path, "Menu1")
         embed = renderer.render("test_key1")
         self.assertEqual(embed.title, "Hello World")
 
-    def test_full_render(self):
+    def test_xml_full_render(self):
         path = "tests/routes/menus.xml"
         renderer = MenuRenderer(path, "Menu1")
         embed = renderer.render("test_key2")
         self.assertEqual(embed.title, "Hello Planet")
 
-    def test_menu_key_not_exist(self):
+    def test_xml_menu_key_not_exist(self):
         path = "tests/routes/menus.xml"
         self.assertRaises(KeyError, MenuRenderer, path, "not_a_key")
 
-    def test_pages(self):
+    def test_xml_pages(self):
         path = "tests/routes/menus.xml"
         renderer = MenuRenderer(path, "Menu1")
         self.assertEqual(renderer.number_of_pages, 2)
 
-    def test_keys(self):
+    def test_xml_keys(self):
         path = "tests/routes/menus.xml"
+        renderer = MenuRenderer(path, "Menu1")
+        self.assertEqual(renderer.keys, ["test_key1", "test_key2"])
+
+
+    def test_json_render(self):
+        path = "tests/routes/menus.json"
+        renderer = MenuRenderer(path, "Menu1")
+        embed = renderer.render("test_key1")
+        self.assertEqual(embed.title, "Hello World")
+
+    def test_json_full_render(self):
+        path = "tests/routes/menus.json"
+        renderer = MenuRenderer(path, "Menu1")
+        embed = renderer.render("test_key2")
+        self.assertEqual(embed.title, "Hello Planet")
+
+    def test_json_menu_key_not_exist(self):
+        path = "tests/routes/menus.json"
+        self.assertRaises(KeyError, MenuRenderer, path, "not_a_key")
+
+    def test_json_pages(self):
+        path = "tests/routes/menus.json"
+        renderer = MenuRenderer(path, "Menu1")
+        self.assertEqual(renderer.number_of_pages, 2)
+
+    def test_json_keys(self):
+        path = "tests/routes/menus.json"
         renderer = MenuRenderer(path, "Menu1")
         self.assertEqual(renderer.keys, ["test_key1", "test_key2"])
