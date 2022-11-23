9aed0ba6f2e13cef414a92f2eca0de3bde821998
Yousef Zaher <45167695+YousefEZ@users.noreply.github.com>
2022-11-23 23:14:21 +0000

    :sparkles: made select component rendering

--- qalib/renderers/file_renderers/_item_wrappers.py
+++ qalib/renderers/file_renderers/_item_wrappers.py
@@ -2,6 +2,7 @@ from typing import Optional, Union
 
 import discord.emoji
 import discord.ui as ui
+import discord.utils
 from discord.ui.button import ButtonStyle
 
 
@@ -30,10 +31,22 @@ def make_emoji(emoji: Optional[Union[str, dict]]) -> Optional[Union[str, discord
 def create_button(**kwargs) -> ui.Button:
     return ui.Button(
         style=make_button_style(kwargs.get("style")),
-        label=kwargs.get("label"),
-        disabled=kwargs.get("disabled", "False").lower() == "True",
         custom_id=kwargs.get("custom_id"),
+        label=kwargs.get("label"),
+        disabled=kwargs.get("disabled", "false").lower() == "true",
         url=kwargs.get("url"),
         emoji=make_emoji(kwargs.get("emoji")),
+        row=int(row) if (row := kwargs.get("row")) is not None else None
+    )
+
+
+def create_select(**kwargs) -> ui.Select:
+    return ui.Select(
+        custom_id=kwargs.get("custom_id"),
+        placeholder=kwargs.get("placeholder", discord.utils.MISSING),
+        min_values=int(kwargs.get("min_values", 1)),
+        max_values=int(kwargs.get("max_values", 1)),
+        disabled=kwargs.get("disabled", "false").lower() == "true",
+        options=kwargs.get("options", []),
         row=int(row) if (row := kwargs.get("row")) is not None else None
     )
