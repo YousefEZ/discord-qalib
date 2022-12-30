129924fca6389903db979430d818ee578acb0ca5
Yousef Zaher <45167695+YousefEZ@users.noreply.github.com>
2022-12-30 15:39:17 +0000

    :wrench: update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import find_packages, setup
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='Discord-Qalib',
     author="Yousef Zaher",
@@ -8,4 +11,7 @@ setup(
     version='0.0.7',
     description='A library for templating responses on .xml, and .json files for discord.py',
     packages=find_packages(exclude=("test*",)),
+    license='MIT',
+    python_requires='>=3.8.0',
+    install_requires=requirements,
 )
