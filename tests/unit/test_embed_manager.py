cc7aad434c3c81f0da4b4430a11a987d1e5fc2f0
Yousef Zaher <45167695+YousefEZ@users.noreply.github.com>
2022-12-31 18:26:04 +0000

    :white_check_mark: add new tests

--- tests/unit/test_embed_manager.py
+++ tests/unit/test_embed_manager.py
@@ -1,11 +1,12 @@
 import datetime
 import unittest
+from typing import cast
 
 import discord.ext.commands
 import jinja2
 
 import qalib.qalib_context
-from qalib import EmbedManager, jinja_manager, menu_manager, embed_manager as embed_decorator
+from qalib import EmbedManager, jinja_manager, embed_manager as embed_decorator, JinjaManager
 from tests.unit.mocked_classes import ContextMocked, MessageMocked
 
 
@@ -47,11 +48,26 @@ class TestEmbedManager(unittest.IsolatedAsyncioTestCase):
         await embed_manager.display("test_key2", keywords={"todays_date": datetime.datetime.now()})
         self.assertEqual(embed_manager._displayed.embed.title, "Test2")
 
+    async def test_xml_rendered_send_message(self):
+        embed_manager = EmbedManager(self.ctx, "tests/routes/full_embeds.xml")
+
+        e = await embed_manager.rendered_send("test_key", keywords={"todays_date": datetime.datetime.now()})
+        self.assertEqual(e.embed.title, "Test")
+
+        e2 = await embed_manager.rendered_send("test_key2", keywords={"todays_date": datetime.datetime.now()})
+        self.assertEqual(e2.embed.title, "Test2")
+
     async def test_xml_display_message_with_buttons(self):
         embed_manager = EmbedManager(self.ctx, "tests/routes/full_embeds.xml")
 
         await embed_manager.display("test_key2", keywords={"todays_date": datetime.datetime.now()})
         self.assertEqual(len(embed_manager._displayed.view.children), 5)
+
+    async def test_xml_menu_display(self):
+        embed_manager = EmbedManager(self.ctx, "tests/routes/menus.xml", "Menu1")
+
+        await embed_manager.menu()
+        self.assertEqual(embed_manager._displayed.embed.title, "Hello World")
 
     async def test_json_embed_manager(self):
         embed_manager = EmbedManager(self.ctx, "tests/routes/simple_embeds.json")
@@ -84,6 +100,35 @@ class TestEmbedManager(unittest.IsolatedAsyncioTestCase):
         await embed_manager.display("test_key2", keywords={"todays_date": datetime.datetime.now()})
         self.assertEqual(len(embed_manager._displayed.view.children), 5)
 
+    async def test_json_menu_display(self):
+        embed_manager = EmbedManager(self.ctx, "tests/routes/menus.json", "Menu1")
+
+        await embed_manager.menu()
+        self.assertEqual(embed_manager._displayed.embed.title, "Hello World")
+
+    async def test_jinja_menu_display(self):
+        embed_manager = JinjaManager(self.ctx, jinja2.Environment(loader=jinja2.FileSystemLoader("tests/routes")),
+                                     "menus.xml", "Menu1")
+
+        await embed_manager.menu()
+        self.assertEqual(embed_manager._displayed.embed.title, "Hello World")
+
+    async def test_menu_arrows(self):
+        embed_manager = EmbedManager(self.ctx, "tests/routes/menus.xml", "Menu1")
+
+        await embed_manager.menu()
+        arrow = embed_manager._displayed.view.children[0]
+        self.assertIsInstance(arrow, discord.ui.Button)
+
+        mocked_interaction = type("MockedInteraction", (object,), {})
+        mocked_interaction.response = type("MockedResponse", (object,), {})()
+
+        async def edit(embed, view, **kwargs):
+            self.assertEqual(embed.title, "Hello Planet")
+
+        mocked_interaction.response.edit_message = edit
+        await arrow.callback(cast(discord.Interaction, mocked_interaction))
+
     async def test_decorator(self):
         @embed_decorator("tests/routes/simple_embeds.json")
         async def test(ctx):
@@ -91,22 +136,17 @@ class TestEmbedManager(unittest.IsolatedAsyncioTestCase):
 
         await test(ContextMocked())
 
-    async def test_menu_manager(self):
-        @menu_manager("tests/routes/menus.json", "Menu1")
-        async def test(ctx):
-            self.assertIsInstance(ctx, qalib.QalibContext)
-
-        await test(ContextMocked())
-
     async def test_jinja_manager(self):
-        @jinja_manager("jinja-test.xml", "tests/routes")
+        @jinja_manager(jinja2.Environment("tests/routes"), "jinja-test.xml")
         async def test(ctx):
             self.assertIsInstance(ctx, qalib.QalibContext)
 
         await test(self.ctx)
 
-    def test_menu_manager_class(self):
-        self.assertTrue(qalib.MenuManager(self.ctx, "tests/routes/menus.json", "Menu1"))
-
     def test_jinja_manager_class(self):
-        self.assertTrue(qalib.JinjaManager(self.ctx, "jinja-test.xml", jinja2.Environment("tests/routes")))
+        env = jinja2.Environment(loader=jinja2.FileSystemLoader("tests/routes"))
+        self.assertTrue(qalib.JinjaManager(self.ctx, env, "jinja-test.xml"))
+
+    def test_root_key_set(self):
+        embed_manager = EmbedManager(self.ctx, "tests/routes/menus.xml")
+        embed_manager.set_root("Menu1")
