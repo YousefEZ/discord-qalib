817d32190196d7c38b7c09c61152ac0cf170d436
Yousef Zaher <45167695+YousefEZ@users.noreply.github.com>
2022-12-31 18:24:55 +0000

    :fire: remove MenuManager (behaviour merged with Proxies)

--- qalib/renderers/menu_proxy.py
+++
@@ -1,9 +0,0 @@
-from qalib.renderers.file_renderers.renderer_factory import RendererFactory
-from qalib.renderers.renderer_proxy import RendererProxy
-
-
-class MenuProxy(RendererProxy):
-
-    def __init__(self, path: str, menu_key: str):
-        super().__init__(path)
-        self._renderer.set_menu(menu_key)
