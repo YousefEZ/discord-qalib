d51f771557f0804cf270501066d2f44ca850d942
Yousef Zaher <45167695+YousefEZ@users.noreply.github.com>
2023-01-08 20:39:35 +0000

    :zap: changed QalibInteraction → QalibInteractionResponse

--- qalib/interaction.py
+++ qalib/interaction.py
@@ -1,22 +1,26 @@
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
 import discord
 
-from qalib import Renderer
+from qalib.renderer import Renderer
 from qalib.translators import Callback
 
 
-class QalibInteraction(discord.Interaction):
+class QalibInteractionResponse(discord.InteractionResponse):
     """The QalibInteraction class is a subclass of discord.Interaction, and is used to add additional functionality to
     the interaction. It is meant to be used in the on_interaction event, and is responsible for deserializing the
     requested modal and sending it to the user."""
 
     def __init__(self, interaction: discord.Interaction, renderer: Renderer):
         """Constructor method for the QalibInteraction class."""
-        super().__init__(data=interaction.__dict__, state=interaction._state)
+        super().__init__(parent=interaction)
         self._renderer = renderer
 
-    async def respond_with_modal(self, key: str, methods: Dict[str, Callback], keywords: Any) -> None:
+    async def respond_with_modal(
+            self,
+            key: str,
+            methods: Optional[Dict[str, Callback]] = None,
+            keywords: Optional[Dict[str, Any]] = None) -> None:
         """Method that is responsible for templating the document, and then deserializing the requested modal based on
         its key and sending it to the user.
 
@@ -25,4 +29,9 @@ class QalibInteraction(discord.Interaction):
             key (str): key that identifies the modal in the route file
             keywords (Any): keywords that are passed to the modal renderer to format the text
         """
-        return await self.response.send_modal(self._renderer.render_modal(key, methods, keywords))
+        if methods is None:
+            methods = {}
+
+        if keywords is None:
+            keywords = {}
+        return await self.send_modal(self._renderer.render_modal(key, methods, keywords))
