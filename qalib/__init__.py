7cfe3aeac2e17ae0242ea0d9b5006495fcaafb09
Yousef Zaher <45167695+YousefEZ@users.noreply.github.com>
2023-01-08 16:12:22 +0000

    :pencil2: rename qalib_context module into context

--- qalib/__init__.py
+++ qalib/__init__.py
@@ -5,9 +5,13 @@ Extensions to the Rapptz Discord.py library, adding the use of templating on emb
 :license: MIT, see LICENSE for more details.
 """
 from functools import wraps
-from typing import Optional
 
-from .qalib_context import QalibContext
+from .context import QalibContext
+from .interaction import QalibInteraction
+from .renderer import Renderer, RenderingOptions
+from .template_engines.formatter import Formatter
+from .template_engines.jinja2 import Jinja2
+from .template_engines.template_engine import TemplateEngine
 
 __title__ = 'qalib'
 __author__ = 'YousefEZ'
@@ -15,29 +19,37 @@ __license__ = 'MIT'
 __copyright__ = 'Copyright 2022-present YousefEZ'
 __version__ = '1.0.0'
 
-from .renderer import Renderer, RenderingOptions
-from .template_engines.formatter import Formatter
-from .template_engines.jinja2 import Jinja2
-from .template_engines.template_engine import TemplateEngine
 
-
-def qalib_context(template_engine: TemplateEngine, filename: str, renderer_options: Optional[RenderingOptions] = None):
+def qalib_context(template_engine: TemplateEngine, filename: str, *renderer_options: RenderingOptions):
     """This decorator is used to create a QalibContext object, and pass it to the function as it's first argument,
     overriding the default context.
 
     Args:
         template_engine (TemplateEngine): template engine that is used to template the document
         filename (str): filename of the document
-        renderer_options (Optional[RenderingOptions]): options for the renderer
+        renderer_options (RenderingOptions): options for the renderer
 
     Returns (QalibContext): decorated function that takes the Context object and using the extended QalibContext object
     """
-    renderer_instance = Renderer(template_engine, filename, renderer_options)
+    renderer_instance = Renderer(template_engine, filename, *renderer_options)
 
     def manager(func):
         @wraps(func)
         async def wrapper(*args, **kwargs):
             return await func(QalibContext(args[0], renderer_instance), *args[1:], **kwargs)
+
+        return wrapper
+
+    return manager
+
+
+def qalib_interaction(template_engine: TemplateEngine, filename: str, *renderer_options: RenderingOptions):
+    renderer_instance = Renderer(template_engine, filename, *renderer_options)
+
+    def manager(func):
+        @wraps(func)
+        async def wrapper(*args, **kwargs):
+            return await func(QalibInteraction(args[0], renderer_instance), *args[1:], **kwargs)
 
         return wrapper
 
