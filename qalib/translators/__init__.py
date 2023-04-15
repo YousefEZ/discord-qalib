2376f077c269e32b3435ec9aea2ad7b02fac2281
Yousef Zaher <45167695+YousefEZ@users.noreply.github.com>
2023-04-16 02:33:46 +0300

    :zap: remove MISSING and replacer with Optional

--- qalib/translators/__init__.py
+++ qalib/translators/__init__.py
@@ -1,51 +1,50 @@
 from dataclasses import dataclass
-from typing import Callable, Awaitable, Optional, Sequence, Union
+from typing import Any, Awaitable, Callable, Dict, Optional, Sequence, TypeVar, Union
 
 import discord
-from discord.utils import MISSING
+from discord.abc import Snowflake
 
-Callback = Callable[[discord.Interaction], Awaitable[None]]
+V_co = TypeVar("V_co", bound=discord.ui.View, covariant=True)
+
+Callback = Callable[[discord.ui.Item[V_co], discord.Interaction], Awaitable[None]]
 
 
 @dataclass
-class DiscordIdentifier:
+class DiscordIdentifier(Snowflake):
+    # pylint: disable=invalid-name
     id: int
 
 
+# pylint: disable=too-many-instance-attributes
 @dataclass
 class Message:
-    """NamedTuple that represents the display of the message.
+    """Dataclass that represents the display of the message.
 
     Look at https://discordpy.readthedocs.io/en/latest/api.html?highlight=send#discord.abc.Messageable.send
     """
-    content: Optional[str] = MISSING
-    tts: bool = MISSING
-    embed: Optional[discord.Embed] = MISSING
-    embeds: Optional[Sequence[discord.Embed]] = MISSING
-    file: Optional[discord.File] = MISSING
-    files: Optional[Sequence[discord.File]] = MISSING
-    stickers: Optional[Sequence[Union[discord.GuildSticker, discord.StickerItem]]] = MISSING
-    delete_after: Optional[float] = MISSING
-    nonce: Optional[Union[str, int]] = MISSING
-    allowed_mentions: Optional[discord.AllowedMentions] = MISSING
-    reference: Optional[Union[discord.Message, discord.MessageReference, discord.PartialMessage]] = MISSING
-    mention_author: Optional[bool] = MISSING
-    view: Optional[discord.ui.View] = MISSING
-    suppress_embeds: bool = MISSING
-    ephemeral: bool = MISSING
+
+    content: Optional[str]
+    tts: Optional[bool]
+    embed: Optional[discord.Embed]
+    embeds: Optional[Sequence[discord.Embed]]
+    file: Optional[discord.File]
+    files: Optional[Sequence[discord.File]]
+    stickers: Optional[Sequence[Union[discord.GuildSticker, discord.StickerItem]]]
+    delete_after: Optional[float]
+    nonce: Optional[Union[str, int]]
+    allowed_mentions: Optional[discord.AllowedMentions]
+    reference: Optional[Union[discord.Message, discord.MessageReference, discord.PartialMessage]]
+    mention_author: Optional[bool]
+    view: Optional[discord.ui.View]
+    suppress_embeds: Optional[bool]
+    ephemeral: Optional[bool]
+
+    def dict(self) -> Dict[str, Any]:
+        return {key: value for key, value in self.__dict__.items() if value is not None}
 
     def __iter__(self):
         # Order is preserved in Python 3.7+: https://mail.python.org/pipermail/python-dev/2017-December/151283.html
         for value in self.__dict__.values():
-            if value is MISSING:
+            if value is None:
                 continue
             yield value
-
-    def keys(self):
-        for key, value in self.__dict__.items():
-            if value is MISSING:
-                continue
-            yield key
-
-    def __getitem__(self, item):
-        return getattr(self, item)
