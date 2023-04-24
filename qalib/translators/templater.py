64696ddc7743a78f0eb699f20e63381c8db6d789
Yousef Zaher <45167695+YousefEZ@users.noreply.github.com>
2023-04-25 02:22:54 +0300

    :pencil2: rename parser to templater

--- qalib/translators/templater.py
+++ qalib/translators/templater.py
@@ -1,18 +1,14 @@
 from __future__ import annotations
 
-from abc import ABC, abstractmethod
-from typing import Any, Dict, Generic, TypeVar
+from typing import Any, Dict, Protocol
 
 from qalib.template_engines.template_engine import TemplateEngine
 
-K = TypeVar("K", bound=str, contravariant=True)
 
-
-class Parser(ABC, Generic[K]):
+class Templater(Protocol):
     """Protocol that represents the parser. It is meant to be placed into a Renderer, and is responsible for parsing the
     document into a menu and a list of callables."""
 
-    @abstractmethod
     def __init__(self, source: str):
         """This method is used to initialize the parser by parsing the source text.
 
@@ -21,13 +17,11 @@ class Parser(ABC, Generic[K]):
         """
         raise NotImplementedError
 
-    @abstractmethod
-    def template(self, key: K, template_engine: TemplateEngine, keywords: Dict[str, Any]) -> str:
+    def template(self, template_engine: TemplateEngine, keywords: Dict[str, Any]) -> str:
         """This method is used to template the embed by first retrieving it using its key, and then templating it using
         the template_engine
 
         Args:
-            key (str): key of the embed
             template_engine (TemplateEngine): template engine that is used to template the embed
             keywords (Dict[str, Any]): keywords that are used to template the embed
 
