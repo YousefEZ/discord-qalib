644b0932eb914c0b12593f52ffdc36944d7630ec
AutoPyPi <auto-pypi[bot]@users.noreply.github.com>
2023-05-05 16:45:23 +0000

    bump version to 2.2.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author="Yousef Zaher",
     author_email="syberprojects@gmail.com",
     url="https://github.com/YousefEZ/discord-qalib",
-    version="2.2.1",
+    version="2.2.2",
     description="A library for templating responses on .xml, and .json files for discord.py",
     packages=find_packages(exclude=("test*",)),
     license="MIT",
